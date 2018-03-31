c6ba605febd0525e9f5919fa098f91d102231339
BuildTools <jmeredit@ashland.edu>
2018-03-31 17:49:07 -0400

    Got a lot of it working

--- src/Vend.java
+++ src/Vend.java
@@ -18,11 +18,21 @@ public class Vend {
 			costThree,
 			costFour,
 			costFive;
+				
+		int money = 0;
 		
-		//Prep Scanner
+		String choice;
+		
+		//Prep Human Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		//Prep File Scanner
 		File file = new File("stock.txt");
 		Scanner input = new Scanner(file);
 	    
+		System.out.println("Welcome to the virtual vending machine."
+				+ "  It serves:\n");
+		
 	    //Get names and cost of items
 	    itemOne = input.nextLine();
 	    costOne = input.nextFloat();
@@ -42,12 +52,136 @@ public class Vend {
 	    //Close input
 		input.close();
 		
-		System.out.println("1: " + itemOne + " $" + costOne);
-		System.out.println("2: " + itemTwo + " $" + costTwo);
-		System.out.println("3: " + itemThree + " $" + costThree);
-		System.out.println("4: " + itemFour + " $" + costFour);
-		System.out.println("5: " + itemFive + " $" + costFive);
+		System.out.printf("1: " + itemOne + " $" + costOne + "\n");
+		System.out.printf("2: " + itemTwo + " $" + costTwo + "\n");
+		System.out.printf("3: " + itemThree + " $" + costThree + "\n");
+		System.out.printf("4: " + itemFour + " $" + costFour + "\n");
+		System.out.printf("5: " + itemFive + " $" + costFive + "\n");
+		
+		//Get all of the users money. They dont need it.
+		money = getMoney(scan);
+		
+		//Get what item the user wants. Make sure that they will eat 
+		//it or I will shove it down their throat.
+		choice = userChoice(scan);
+		
+		//Calculate cost of item and see if the user has enough money
+		switch (choice) {
+			case "1":
+				calculateCost(money, itemOne, costOne);
+				break;
+			case "2":
+				calculateCost(money, itemTwo, costTwo);
+				break;
+			case "3":
+				calculateCost(money, itemThree, costThree);
+				break;
+			case "4":
+				calculateCost(money, itemFour, costFour);
+				break;
+			case "5":
+				calculateCost(money, itemFive, costFive);
+				break;
+		}
+		
+		//Test to make sure this is working
+		System.out.println(money);
+		
 	}
 	
+	public static void 
 	
+	public static String userChoice (Scanner scan) {
+		int choiceFromUser;
+		String choice = "e";
+		System.out.println("Please enter your selection: ");
+		choiceFromUser = scan.nextInt();
+		while ((choiceFromUser > 5) || (choiceFromUser < 1)) {
+			System.out.println("Please enter a valid choice (1-5): ");
+			choiceFromUser = scan.nextInt();
+		}
+		if (choiceFromUser == 1) {
+			choice = "1";
+			return choice;
+		} else if (choiceFromUser == 2) {
+			choice = "2";
+			return choice;
+		} else if (choiceFromUser == 3) {
+			choice = "3";
+			return choice;
+		} else if (choiceFromUser == 4) {
+			choice = "4";
+			return choice;
+		} else if (choiceFromUser == 5) {
+			choice = "5";
+			return choice;
+		}
+		return choice;
+	}
+	
+	public static int getMoney (Scanner scan) {
+		int money;
+		System.out.println("");
+		System.out.println("Please enter the amount of money "
+				+ "deposited (in dollars) : $");
+		
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//GET HER TO SEE IF THIS IS OK
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		//
+		money = (int) (scan.nextFloat() * 100);
+		return money;
+	}
 }
---
+++ src/test.java
@@ -0,0 +1,12 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		String test;
+		int x = 0;
+		while (x<1) {
+			test = "test";
+		}
+	}
+
+}
