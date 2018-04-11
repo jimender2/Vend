57efc443896768f5d49546c0fe354296087d0032
BuildTools <jmeredit@ashland.edu>
2018-04-11 16:21:36 -0400

    Finished? Need to add comments

--- src/Vend.java
+++ src/Vend.java
@@ -6,22 +6,24 @@ public class Vend {
 
 	public static void main(String[] args) throws IOException {
 		
-		//Declare variables
+		//Declare boolean variables
+		boolean again = false;
+		
 		String itemOne,
 			itemTwo,
 			itemThree,
 			itemFour,
-			itemFive;
+			itemFive,
+			user,
+			choice;
 		
 		float costOne,
 			costTwo,
 			costThree,
 			costFour,
 			costFive;
-				
-		int money = 0;
 		
-		String choice;
+		int money = 0;
 		
 		//Prep Human Scanner
 		Scanner scan = new Scanner(System.in);
@@ -29,131 +31,140 @@ public class Vend {
 		//Prep File Scanner
 		File file = new File("stock.txt");
 		Scanner input = new Scanner(file);
-	    
-		System.out.println("Welcome to the virtual vending machine."
+
+		//Get names and cost of items
+		itemOne = input.nextLine();
+		costOne = input.nextFloat();
+		input.nextLine();
+		itemTwo = input.nextLine();
+		costTwo = input.nextFloat();
+		input.nextLine();
+		itemThree = input.nextLine();
+		costThree = input.nextFloat();
+		input.nextLine();
+		itemFour = input.nextLine();
+		costFour = input.nextFloat();
+		input.nextLine();
+    	itemFive = input.nextLine();
+    	costFive = input.nextFloat();
+    
+    	//Close input
+    	input.close();
+    	
+		do {
+			System.out.println("Welcome to the virtual vending machine."
 				+ "  It serves:\n");
-		
-	    //Get names and cost of items
-	    itemOne = input.nextLine();
-	    costOne = input.nextFloat();
-	    input.nextLine();
-	    itemTwo = input.nextLine();
-	    costTwo = input.nextFloat();
-	    input.nextLine();
-	    itemThree = input.nextLine();
-	    costThree = input.nextFloat();
-	    input.nextLine();
-	    itemFour = input.nextLine();
-	    costFour = input.nextFloat();
-	    input.nextLine();
-	    itemFive = input.nextLine();
-	    costFive = input.nextFloat();
-	    
-	    //Close input
-		input.close();
 				
-		System.out.printf("1: %-20s $%.2f \n", itemOne, costOne);
-		System.out.printf("2: %-20s $%.2f \n", itemTwo, costTwo);
-		System.out.printf("3: %-20s $%.2f \n", itemThree, costThree);
-		System.out.printf("4: %-20s $%.2f \n", itemFour, costFour);
-		System.out.printf("5: %-20s $%.2f \n", itemFive, costFive);
+	    	System.out.printf("1: %-20s $%.2f \n", itemOne, costOne);
+	    	System.out.printf("2: %-20s $%.2f \n", itemTwo, costTwo);
+	    	System.out.printf("3: %-20s $%.2f \n", itemThree, costThree);
+	    	System.out.printf("4: %-20s $%.2f \n", itemFour, costFour);
+	    	System.out.printf("5: %-20s $%.2f \n", itemFive, costFive);
 		
-		//Get all of the users money. They dont need it.
-		money = getMoney(scan);
+	    	//Get all of the users money. They dont need it.
+	    	money = getMoney(scan);
 		
-		//Get what item the user wants. Make sure that they will eat 
-		//it or I will shove it down their throat.
-		choice = userChoice(scan);
+	    	//Get what item the user wants. Make sure that they will eat 
+	    	//it or I will shove it down their throat.
+	    	choice = userChoice(scan);
 		
-		//Calculate cost of item and see if the user has enough money
-		switch (choice) {
-			case "1":
-				calculateCost(money, itemOne, costOne);
-				break;
-			case "2":
-				calculateCost(money, itemTwo, costTwo);
-				break;
-			case "3":
-				calculateCost(money, itemThree, costThree);
-				break;
-			case "4":
-				calculateCost(money, itemFour, costFour);
-				break;
-			case "5":
-				calculateCost(money, itemFive, costFive);
-				break;
-			default:
-				break;
-		}
-		
-		//Test to make sure this is working
-		
+	    	//Calculate cost of item and see if the user has enough money
+	    	switch (choice) {
+	    		case "1":
+	    			calculateCost(money, itemOne, costOne);
+	    			break;
+	    		case "2":
+	    			calculateCost(money, itemTwo, costTwo);
+	    			break;
+	    		case "3":
+	    			calculateCost(money, itemThree, costThree);
+	    			break;
+	    		case "4":
+	    			calculateCost(money, itemFour, costFour);
+					break;
+	    		case "5":
+	    			calculateCost(money, itemFive, costFive);
+	    			break;
+	    		default:
+	    			break;
+	    	}
+	    	
+	    	System.out.println("Would you like to make another purchase? "
+	    			+ "(y/n)");
+	    	scan.nextLine();
+	    	user = scan.nextLine();
+	    	user = user.toLowerCase();
+	    	switch (user) {
+	    		case "y":
+	    			again = true;
+	    			break;
+	    		case "n":
+	    			again = false;
+	    			break;
+	    		default:
+	    			again = false;
+	    	}
+		} while (again);
+		scan.close();
 	}
 	
 	public static void calculateCost(int changeMoney, String item, float cost) {
-		int costConvert,
-			quarters,
+		int	quarters,
 			dimes,
-			pennies;
+			pennies,
+			change;
 		
 		double money;
 		
-		System.out.println(changeMoney);
-				
 		money = (changeMoney + 0.0) / 100;
-		
-		System.out.println(money);
-		
-		System.out.println(money);
 		
 		if (money < cost) {
 			System.out.println("You entered insufficient funds to purchase "
 					+ "the " + item + ".");
 		} else {
-			costConvert = (int) (cost * 100);
-			money = money - costConvert;			
+			money = money - cost;			
 		}
 		
-		money = money * 100;
-		
-		quarters = findChange(money, 25);
-		dimes = findChange(money, 10);
-		pennies = findChange(money, 1);
+		change = (int) (money * 100);
+				
+		quarters = findChange(change, 25);
+		dimes = findChange(change, 10);
+		pennies = findChange(change, 1);
 		
 		System.out.println("I have returned " + quarters + " quarters, "
 				+ dimes + " dimes, " + pennies + " pennies to you.");
 		
 	}
 	
-	public static int findChange(double changeMoney, int denomination) {
-		int change,
-			quarters,
+	public static int findChange(int money, int denomination) {
+		int quarters,
 			dimes,
 			pennies;
 		
-		change = 0;
+		//Debug
+		System.out.println(money);
 		
-//		//Quarters
-//		quarters = changeMoney / 25;
-//		changeMoney = changeMoney % 25;
-//		
-//		//Dimes
-//		dimes = changeMoney / 10;
-//		changeMoney = changeMoney % 10;
-//		
-//		//Pennies
-//		pennies = changeMoney / 1;
-//		
-//		if (denomination == 25) {
-//			change = quarters;
-//		} else if (denomination == 10) {
-//			change = dimes;
-//		} else if (denomination == 1) {
-//			change = pennies;
-//		} else {
-//			change = 0;
-//		}
-		return change;
+		//Quarters
+		quarters = (money / 25);
+		
+		money = money - (quarters * 25);
+		
+		//Dimes
+		dimes = (money / 10);
+
+		money = money - (dimes * 10);
+		
+		//Pennies
+		pennies = (money / 1);
+		
+		if (denomination == 25) {
+			return quarters;
+		} else if (denomination == 10) {
+			return dimes;
+		} else if (denomination == 1) {
+			return pennies;
+		}
+		return money;
 	}
 	
 	public static String userChoice (Scanner scan) {
